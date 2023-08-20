1ec5a86da103c721a14104b4a1ffb6ae565b75d4
Gokulsenthil <gokulsenthil899@gmail.com>
2023-08-20 22:44:21 +0530

    mangoo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
-console.log("Hello World");
-
+// imports from packages
 const express = require("express");
-const PORT = 3000;
+const mongoose = require('mongoose');
+// import files from other files
+const authRouter = require('./routes/auth');
 
+// init
+
+const PORT = 3000;
 const app = express();
-// something able to replace a () => {} as a function
-app.listen(PORT, "0.0.0.0", () =>{
+const DB =
+   "mongodb+srv://gokul:TomGokul6379216315@cluster0.2mbtukr.mongodb.net/?retryWrites=true&w=majority"
+// middleware
+
+app.use(authRouter);
+
+// connections
+
+mongoose
+.connect(DB)
+.then(() => {
+    console.log("Connection successfully");
+})
+.catch((e) => {
+console.log(e);
+    });
+
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`connected  at port ${PORT}`);
 
-})
+});
